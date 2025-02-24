a627fb240c957a786df28f03555655a1764c6b88
penguinawesome1 <owen.h.colley@gmail.com>
2025-02-24 15:30:32 -0500

    Add files via upload

---
+++ js/classes/Life.js
@@ -0,0 +1,65 @@
+class Life extends Sprite {
+    constructor({
+        position,
+        collisionBlocks,
+        imageSrc,
+        frameRate = 1,
+        scale = 1,
+        animations
+    }) {
+        super({ position, imageSrc, frameRate, scale });
+
+        this.position = position;
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+        this.collisionBlocks = collisionBlocks;
+        this.hitbox = {
+            position: this.position,
+            width: 0,
+            height: 0,
+        };
+
+        for (let key in this.animations) {
+            const image = new Image();
+            image.src = this.animations[key].imageSrc;
+
+            this.animations[key].image = image;
+        }
+    }
+
+    switchSprite(key) {
+        const sameImage = this.image === this.animations[key].image;
+        if (sameImage || !this.loaded) return;
+
+        this.currentFrame = 0;
+        this.image = this.animations[key].image;
+        this.frameBuffer = this.animations[key].frameBuffer;
+        this.frameRate = this.animations[key].frameRate;
+    }
+
+    applyGravity() {
+        this.velocity.y += gravity;
+        this.position.y += this.velocity.y;
+    }
+
+    applyFriction() {
+        this.velocity.x *= frictionMultiplier;
+        this.position.x += this.velocity.x;
+    }
+
+    isCollision() {
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i];
+
+            if (collision({
+                object1: this.hitbox,
+                object2: collisionBlock,
+            })) {
+                return collisionBlock;
+            }
+        }
+        return null;
+    }
+}
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,75 +1,30 @@
-class Player extends Component {
+class Player extends Life {
     constructor({
         position,
         collisionBlocks,
-        platformCollisionBlocks,
         imageSrc,
         frameRate,
-        scale = .4,
+        scale = 1,
         animations
     }) {
-        super({ position, imageSrc, frameRate, scale });
-        this.position = position;
-        this.velocity = {
-            x: 0,
-            y: 0,
-        }
-        this.collisionBlocks = collisionBlocks;
-        this.platformCollisionBlocks = platformCollisionBlocks;
-        this.hitbox = {
-            position: this.position,
-            width: 10,
-            height: 10,
-        }
-
-        this.otherPlayer = null;
-        this.healthBar = null;
-        this.lives = maxLives;
-        this.jumps = maxJumps;
-        this.dashes = maxDashes;
-        this.lastDirection = "right";
-        this.animations = animations;
-
-        for (let key in this.animations) {
-            const image = new Image();
-            image.src = this.animations[key].imageSrc;
-
-            this.animations[key].image = image;
-        }
-
+        super({ position, imageSrc, frameRate, scale, animations });
+        
         this.attackBox = {
             position: this.position,
             width: 0,
             height: 0,
         }
 
+        this.jumps = true;
+        this.lastDirection = "right";
+        this.animations = animations;
+        
         this.keys = {
             left: false,
             right: false,
             down: false,
             up: false,
         };
-
-        this.attackList = [];
-
-        this.attackImages = new Set([
-            this.animations.Attack1.image,
-            this.animations.Attack1Left.image,
-            this.animations.Attack2.image,
-            this.animations.Attack2Left.image,
-        ]);
-    }
-
-    switchSprite(key) {
-        const isAttacking = this.attackImages.has(this.image);
-        const notLastFrame = this.currentFrame < this.animations.Attack1.frameRate - 1;
-        const sameImage = this.image === this.animations[key].image;
-        if (sameImage || !this.loaded || (isAttacking && notLastFrame)) return;
-
-        this.currentFrame = 0;
-        this.image = this.animations[key].image;
-        this.frameBuffer = this.animations[key].frameBuffer;
-        this.frameRate = this.animations[key].frameRate;
     }
 
     update() {
@@ -82,137 +37,92 @@ class Player extends Component {
         
         this.applyFriction();
         this.updateHitbox();
-        this.respondToHorizontalCollision();
+        // this.respondToHorizontalCollision();S
 
-        this.applyGravity();
-        this.updateHitbox();
-        this.grounded = this.respondToVerticalCollision() !== false;
-        this.crouching = this.grounded && this.keys.down;
-
-        this.checkForHit();
-        this.checkForDeath();
-    }
-
-    jump() {
-        if (this.jumps < 1 || this.crouching) return;
-        this.velocity.y = -jumpStrength;
-        this.jumps--;
-    }
-
-    applyGravity() {
-        this.velocity.y += gravity;
-        this.position.y += this.velocity.y;
+        // this.checkForHit();
+        // this.checkForDeath();
     }
 
     applyFriction() {
         this.velocity.x *= frictionMultiplier;
+        this.velocity.y *= frictionMultiplier;
         this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
     }
 
     checkForHit() {
-        if (!this.isAttacking || this.otherPlayer.dashing) return;
+        // if (!this.isAttacking || this.otherPlayer.dashing) return;
 
-        if (collision({
-            object1: this.attackBox,
-            object2: this.otherPlayer.hitbox,
-        })) {
-            const angle = calcAngle({
-                object1: this.attackBox,
-                object2: this.otherPlayer.hitbox,
-            });
-            this.otherPlayer.velocity.x += Math.cos(angle) * 2000 / this.otherPlayer.healthBar.value;
-            this.otherPlayer.velocity.y += Math.sin(angle) * 700 / this.otherPlayer.healthBar.value;
-
-            this.hitStop = true;
-            this.otherPlayer.hitStop = true;
-            setTimeout(() => {
-                this.hitStop = false;
-                this.otherPlayer.hitStop = false;
-            }, hitStopDuration);
-
-            this.otherPlayer.healthBar.value -= 10;
-            this.isAttacking = false;
-        }
+        // if (collision({
+        //     object1: this.attackBox,
+        //     object2: this.otherPlayer.hitbox,
+        // })) {
+        //     const angle = calcAngle({
+        //         object1: this.attackBox,
+        //         object2: this.otherPlayer.hitbox,
+        //     });
+        //     this.otherPlayer.velocity.x += Math.cos(angle) * 2000 / this.otherPlayer.healthBar.value;
+        //     this.otherPlayer.velocity.y += Math.sin(angle) * 700 / this.otherPlayer.healthBar.value;
+        // }
     }
 
     updateHitbox() {
-        if (this.crouching || this.keys.up || this.velocity.y < 0) {
-            this.hitbox = {
-                position: {
-                    x: this.position.x + 44 * this.scale,
-                    y: this.position.y + 110 * this.scale / 2,
-                },
-                width: 70 * this.scale,
-                height: 110 * this.scale / 2,
-            }
-        } else {
-            this.hitbox = {
-                position: {
-                    x: this.position.x + 44 * this.scale,
-                    y: this.position.y,
-                },
-                width: 70 * this.scale,
-                height: 110 * this.scale,
-            }
+        this.hitbox = {
+            position: {
+                x: this.position.x + this.width / 2,
+                y: this.position.y + this.height / 2,
+            },
+            width: 1 * this.scale,
+            height: 1 * this.scale,
         }
         
-        if (this.dashing) {
-            this.attackBox.width = 0;
-        } else if (this.keys.up) {
-            this.attackBox = {
-                position: {
-                    x: this.position.x + this.scale * 70,
-                    y: this.position.y - 21 * this.scale,
-                },
-                width: 20 * this.scale,
-                height: 20 * this.scale,
-            }
-        } else {
-            this.attackBox = {
-                position: {
-                    x: this.position.x + this.scale * (28 + 52 * (this.attackDirection === "right" ? 1 : 0)),
-                    y: this.position.y + 23 * this.scale,
-                },
-                width: 53 * this.scale,
-                height: 20 * this.scale,
-            }
+        this.attackBox = {
+            position: {
+                x: this.position.x + this.scale * (28 + 52 * (this.attackDirection === "right" ? 1 : 0)),
+                y: this.position.y + 23 * this.scale,
+            },
+            width: 53 * this.scale,
+            height: 20 * this.scale,
         }
     }
 
-    checkForKeys() {
-        let sprite = "Idle";
-        
-        if (this.keys.right && !this.keys.left) {
-            sprite = "Run";
-            this.velocity.x += playerSpeed;
-            if (!this.isAttacking) this.lastDirection = "right";
-        } else if (this.keys.left && !this.keys.right) {
-            sprite = "RunLeft";
-            this.velocity.x += -playerSpeed;
-            if (!this.isAttacking) this.lastDirection = "left";
-        } else if (this.velocity.y === 0) {
-            if (this.lastDirection === "right") {
-                sprite = "Idle";
-            } else {
-                sprite = "IdleLeft";
-            }
+    checkForKeys() {        
+        const v = {
+            x: 0,
+            y: 0,
         }
-    
-        if (this.velocity.y < 0) {
-            if (this.lastDirection === "right") {
-                sprite = "Jump";
-            } else {
-                sprite = "JumpLeft";
-            }
-        } else if (this.velocity.y > 0) {
-            if (this.lastDirection === "right") {
-                sprite = "Fall";
-            } else {
-                sprite = "FallLeft";
-            }
-        }
+        if (this.keys.left) v.x--;
+        if (this.keys.right) v.x++;
+        if (this.keys.up) v.y--;
+        if (this.keys.down) v.y++;
 
-        this.switchSprite(sprite);
+        if (v.x !== 0 && v.y !== 0) {
+            v.x *= .7;
+            v.y *= .7;
+        }
+        console.log(v.x, v.y, this.keys.left)
+
+        this.velocity.x += v.x;
+        this.velocity.y += v.y;
+
+        // let sprite = "Idle";
+        // if (this.keys.right && !this.keys.left) {
+        //     sprite = "Run";
+        //     this.velocity.x += playerSpeed;
+        //     if (!this.isAttacking) this.lastDirection = "right";
+        // } else if (this.keys.left && !this.keys.right) {
+        //     sprite = "RunLeft";
+        //     this.velocity.x += -playerSpeed;
+        //     if (!this.isAttacking) this.lastDirection = "left";
+        // } else if (this.velocity.y === 0) {
+        //     if (this.lastDirection === "right") {
+        //         sprite = "Idle";
+        //     } else {
+        //         sprite = "IdleLeft";
+        //     }
+        // }
+
+        // this.switchSprite(sprite);
     }
 
     respondToHorizontalCollision() {
@@ -236,28 +146,9 @@ class Player extends Component {
 
     respondToVerticalCollision() {
         const collisionBlock = this.isCollision();
-        const platformCollisionBlock = this.isPlatformCollision();
-        if (collisionBlock) {
-            if (this.velocity.y > 0) {
-                this.velocity.y = 0;
-                this.jumps = maxJumps;
-                this.dashes = maxDashes;
-                this.smashing = false;
-    
-                const offset = this.hitbox.position.y - this.position.y + this.hitbox.height;
-    
-                this.position.y = collisionBlock.position.y - offset - 0.01;
-            } else if (this.velocity.y < 0) {
-                this.velocity.y = 0;
-                
-                const offset = this.hitbox.position.y - this.position.y;
-    
-                this.position.y = collisionBlock.position.y + collisionBlock.height - offset + 0.01;
-            } return true;
-        } else if (platformCollisionBlock) {
-            const jumpDown = this.crouching && this.keys.up;
-            if (jumpDown || this.velocity.y <= 0) return;
+        if (!collisionBlock) return false;
 
+        if (this.velocity.y > 0) {
             this.velocity.y = 0;
             this.jumps = maxJumps;
             this.dashes = maxDashes;
@@ -265,10 +156,15 @@ class Player extends Component {
 
             const offset = this.hitbox.position.y - this.position.y + this.hitbox.height;
 
-            this.position.y = platformCollisionBlock.position.y - offset - 0.01;
-            return true;
+            this.position.y = collisionBlock.position.y - offset - 0.01;
+        } else if (this.velocity.y < 0) {
+            this.velocity.y = 0;
+            
+            const offset = this.hitbox.position.y - this.position.y;
+
+            this.position.y = collisionBlock.position.y + collisionBlock.height - offset + 0.01;
         }
-        return false;
+        return true;
         
     }
 
