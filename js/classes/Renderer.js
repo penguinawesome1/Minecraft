7bb9da99a92d5de25b5e3cf4e8f0bc06b9d58c17
penguinawesome1 <owen.h.colley@gmail.com>
2025-03-15 18:14:20 -0400

    Add files via upload

--- js/classes/Renderer.js
+++ js/classes/Renderer.js
@@ -23,14 +23,9 @@ class Renderer {
   static SPROUT = 19;
   static DIRT = 21;
   static GRASS = 23;
-<<<<<<< HEAD
   static WOOD = 25;
   static MOSS = 27;
   static LEAF = 30;
-=======
-  static FARMLAND = 25;
-  static MOSS = 27;
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
   static SHRUB = 36;
   static COBBLESTONE = 61;
   static STONE = 63;
@@ -38,11 +33,7 @@ class Renderer {
   static BEDROCK = 115;
 
   async setupImages() {
-<<<<<<< HEAD
     this.imageMap = [];
-=======
-    this.imageMap = new Map();
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
     this.imagesLoaded = false;
     await this.loadAllImagesObjects();
     this.imagesLoaded = true;
@@ -58,18 +49,11 @@ class Renderer {
   }
 
   getScaledCanvas() {
-<<<<<<< HEAD
     const zoom = this.zoom;
     return {
       scale: zoom,
       width: this.canvas.width / zoom,
       height: this.canvas.height / zoom,
-=======
-    return {
-      scale: this.zoom,
-      width: this.canvas.width / this.zoom,
-      height: this.canvas.height / this.zoom,
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
     };
   }
 
@@ -86,14 +70,9 @@ class Renderer {
       { src: `../img/tiles/tile_019.png`, blockNum: Renderer.SPROUT },
       { src: `../img/tiles/tile_021.png`, blockNum: Renderer.DIRT },
       { src: `../img/tiles/tile_023.png`, blockNum: Renderer.GRASS },
-<<<<<<< HEAD
       { src: `../img/tiles/tile_025.png`, blockNum: Renderer.WOOD },
       { src: `../img/tiles/tile_027.png`, blockNum: Renderer.MOSS },
       { src: `../img/tiles/tile_030.png`, blockNum: Renderer.LEAF },
-=======
-      { src: `../img/tiles/tile_025.png`, blockNum: Renderer.FARMLAND },
-      { src: `../img/tiles/tile_027.png`, blockNum: Renderer.MOSS },
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
       { src: `../img/tiles/tile_036.png`, blockNum: Renderer.SHRUB },
       { src: `../img/tiles/tile_061.png`, blockNum: Renderer.COBBLESTONE },
       { src: `../img/tiles/tile_063.png`, blockNum: Renderer.STONE },
@@ -101,38 +80,18 @@ class Renderer {
       { src: `../img/tiles/tile_115.png`, blockNum: Renderer.BEDROCK },
     ];
 
-<<<<<<< HEAD
     for (let i = 0; i < imageDefinitions.length; i++) {
       const { src, blockNum: id } = imageDefinitions[i];
       const img = document.createElement("img");
       img.src = src;
       this.imageMap[id] = img;
     }
-=======
-    const promises = imageDefinitions.map(({ src, blockNum }) => {
-      const img = new Image();
-      const promise = new Promise((resolve) => {
-        img.onload = () => {
-          this.imageMap.set(blockNum, img);
-          resolve();
-        };
-      });
-      img.src = src;
-      return promise;
-    });
-
-    await Promise.all(promises);
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
   }
 
   draw({ blockNum, position }) {
     if (blockNum === Renderer.AIR || !this.imagesLoaded) return;
     this.c.drawImage(
-<<<<<<< HEAD
       this.imageMap[blockNum],
-=======
-      this.imageMap.get(Renderer.COBBLESTONE),
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
       position.x,
       position.y - position.z / 2,
       32,
@@ -162,7 +121,6 @@ class Renderer {
   ) {
     this.mouse.screenPosition.x = x;
     this.mouse.screenPosition.y = y;
-<<<<<<< HEAD
     const { x: cameraX, y: cameraY } = this.camera.position;
     const scale = this.scaledCanvas.scale;
     this.mouse.worldPosition.x = x / scale - cameraX;
@@ -175,33 +133,14 @@ class Renderer {
 
     const originalCenterX = cameraX + canvasWidth / 2;
     const originalCenterY = cameraY + canvasHeight / 2;
-=======
-    this.mouse.worldPosition.x =
-      x / this.scaledCanvas.scale - this.camera.position.x;
-    this.mouse.worldPosition.y =
-      y / this.scaledCanvas.scale - this.camera.position.y;
-  }
-
-  handleZoom(delta) {
-    const originalCenterX =
-      this.camera.position.x + this.scaledCanvas.width / 2;
-    const originalCenterY =
-      this.camera.position.y + this.scaledCanvas.height / 2;
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
 
     this.zoom = Math.max(1.2, Math.min(2.8, this.zoom - delta * 0.1)); // Clamp zoom
 
     this.scaledCanvas = this.getScaledCanvas();
-<<<<<<< HEAD
     ({ width: canvasWidth, height: canvasHeight } = this.scaledCanvas);
 
     const newCenterX = cameraX + canvasWidth / 2;
     const newCenterY = cameraY + canvasHeight / 2;
-=======
-
-    const newCenterX = this.camera.position.x + this.scaledCanvas.width / 2;
-    const newCenterY = this.camera.position.y + this.scaledCanvas.height / 2;
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
 
     const offsetX = originalCenterX - newCenterX;
     const offsetY = originalCenterY - newCenterY;
@@ -211,7 +150,6 @@ class Renderer {
   }
 
   panCamera(player) {
-<<<<<<< HEAD
     const { x: cameraX, y: cameraY } = this.camera.position;
     const { width: canvasWidth, height: canvasHeight } = this.scaledCanvas;
     const { x: boxX, y: boxY } = player.cameraBox.position;
@@ -221,21 +159,6 @@ class Renderer {
     const rightBoundary = -cameraX + canvasWidth - boxWidth;
     const topBoundary = -cameraY;
     const bottomBoundary = -cameraY + canvasHeight - boxHeight;
-=======
-    const cameraX = -this.camera.position.x;
-    const cameraY = -this.camera.position.y;
-    const canvasWidth = this.scaledCanvas.width;
-    const canvasHeight = this.scaledCanvas.height;
-    const boxX = player.cameraBox.position.x;
-    const boxY = player.cameraBox.position.y;
-    const boxWidth = player.cameraBox.width;
-    const boxHeight = player.cameraBox.height;
-
-    const leftBoundary = cameraX;
-    const rightBoundary = cameraX + canvasWidth - boxWidth;
-    const topBoundary = cameraY;
-    const bottomBoundary = cameraY + canvasHeight - boxHeight;
->>>>>>> 3ad5e85428332d2d0cf1f655713cc45ef695ef74
 
     if (boxX < leftBoundary) {
       this.camera.position.x = -boxX;
